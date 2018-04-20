84e24c04a4d8050774cb364f24d1a29cc9a8b273
Óscar Martínez <masquesig@gmail.com>
2018-04-21 01:36:16 +0200

    Init tab visibilityorder

--- tabbedtoc.py
+++ tabbedtoc.py
@@ -10,6 +10,7 @@ import sourceorder
 reload(sourceorder)
 
 from sourceorder import setTreeAsSourceOrder
+from org.gvsig.fmap.mapcontext.events.listeners import ViewPortListener
 
 #
 # http://desktop.arcgis.com/es/arcmap/10.3/map/working-with-arcmap/using-the-table-of-contents.htm
@@ -17,13 +18,15 @@ from sourceorder import setTreeAsSourceOrder
 
 from org.gvsig.tools.swing.api import Component
 
-class TabbedToC(FormPanel,Component):
+from visibilityorder import setTreeAsVisibilityOrder
+
+class TabbedToC(FormPanel,Component,ViewPortListener):
   def __init__(self):
     FormPanel.__init__(self,getResource(__file__,"tabbedtoc.xml"))
     self.tabTOC.setToolTipTextAt(0,"Lista por orden de dibujo")
     self.tabTOC.setToolTipTextAt(1,"Lista por fuente")
     self.tabTOC.setToolTipTextAt(2,"Lista por visibilidad")
-    self.tabTOC.setToolTipTextAt(3,unicode("Lista por selecciÃ³n","utf-8"))
+    self.tabTOC.setToolTipTextAt(3,unicode("Lista por selección","utf-8"))
     self.__mapContext = None
     self.setPreferredSize(300,200)
 
@@ -38,9 +41,28 @@ class TabbedToC(FormPanel,Component):
     viewPanel.getViewInformationArea().add(self,"TOC", 0, "ToC", None, "Table of contents")
     
     self.__mapContext = viewDoc.getMapContext()
+    # TAB Source Order
     setTreeAsSourceOrder(self.treeSourceOrder, self.__mapContext)
+    # TAB Visibility 
+    setTreeAsVisibilityOrder(self.treeVisibilityOrder, self.__mapContext)
+    # TAB Selection
     
+    # Agregamos listener al ViewPort
+    self.__mapContext.getViewPort().addViewPortListener(self)
+
     
+  # Metodo obligatorio de ViewPortListener
+  def backColorChanged(self,*args):
+      pass
+
+  # Metodo obligatorio de ViewPortListener
+  def extentChanged(self,*args):
+      setTreeAsVisibilityOrder(self.treeVisibilityOrder, self.__mapContext)
+
+  # Metodo obligatorio de ViewPortListener
+  def projectionChanged(self,*args):
+      pass
+            
 def main(*args):
     panel = TabbedToC()
     panel.install()
--- visibilityorder.py
+++ visibilityorder.py
@@ -2,9 +2,106 @@
 
 import gvsig
 
+# TODO soport para el viewportlistener
+# http://downloads.gvsig.org/download/web/es/build/html/scripting_devel_guide/2.3/capturando_eventos.html
+
+
+from javax.swing.tree import DefaultMutableTreeNode
+from javax.swing.tree import DefaultTreeModel
+import os
+
+def setTreeAsVisibilityOrder(tree, mapContext):
+  model = createTreeModel(mapContext)
+  tree.setModel(model)
+  expandAllNodes(tree, 0, tree.getRowCount())
+
+def expandAllNodes(tree, startingIndex, rowCount):
+    for i in xrange(startingIndex,rowCount): 
+        tree.expandRow(i)
+
+    if tree.getRowCount()!=rowCount:
+        expandAllNodes(tree, rowCount, tree.getRowCount())
+        
+def createTreeModel(mapContext, reducedTree=True):
+    root = DefaultMutableTreeNode("Visibility")
+    rootWithVisibility = DefaultMutableTreeNode(DataFolder("Layers with Visibility",None))
+    rootWithoutVisibility = DefaultMutableTreeNode(DataFolder("Layers without Visibility",None))
+    rootNotVisibility = DefaultMutableTreeNode(DataFolder("Layers desactivated",None))
+    
+    root.insert(rootWithVisibility, root.getChildCount())
+    root.insert(rootWithoutVisibility, root.getChildCount())
+    root.insert(rootNotVisibility, root.getChildCount())
+    
+    yesVis = list()
+    outVis = list()
+    notVis = list()
+    for layer in iter(mapContext.deepiterator()):
+        if layer.isWithinScale(mapContext.getScaleView()) and layer.isVisible():
+            yesVis.append(layer)
+            newNode = DefaultMutableTreeNode(DataLayer(layer.getName(),layer))
+            rootWithVisibility.insert(newNode, rootWithVisibility.getChildCount())
+        elif not layer.isWithinScale(mapContext.getScaleView()) and layer.isVisible():
+            outVis.append(layer)
+            newNode = DefaultMutableTreeNode(DataLayer(layer.getName(),layer))
+            rootWithoutVisibility.insert(newNode, rootWithoutVisibility.getChildCount())
+        elif layer.isVisible()==False:
+            notVis.append(layer)
+            newNode = DefaultMutableTreeNode(DataLayer(layer.getName(),layer))
+            rootNotVisibility.insert(newNode,rootNotVisibility.getChildCount())
+            
+    #for v in yesVis:
+    #    folder = DefaultMutableTreeNode(DataFolder(path,path))
+    #    root.insert(folder, root.getChildCount())
+    """
+    layers.sort(cmp = lambda x,y: cmp(x[0],y[0]))
+    if reducedTree:
+      buildReducedTreeFromLayers(localLayers,layers)
+    else:
+      for path,layer in layers:
+        buildTreeFromPath(localLayers,path,layer)
+    """
+    model = DefaultTreeModel(root)
+    return model
+
+class DataFolder(object):
+  def __init__(self,name, path, icon=None):
+    self.__name = name
+    self.__path = path
+    self.__icon = icon
+  
+  def getName(self):
+    return self.__name
+
+  def __str__(self):
+    return self.__name
+  toString = __str__
+  __repr__ = __str__
+
+  def isLeaf(self):
+    return False
+    
+class DataLayer(object):
+  def __init__(self,path,layer):
+    self.__path = path
+    self.__layer = layer
+    self.__label = os.path.basename(self.__path)
+  def getName(self):
+    return self.__label
+  
+  toString = getName
+  __str__ = getName
+  __repr__ = getName
+
+  def isLeaf(self):
+    return True
+    
 def main(*args):
 
-    #Remove this lines and add here your code
-
-    print "hola mundo"
-    pass
+    layer = gvsig.currentLayer()
+    print layer.getMinScale()
+    print layer.getMaxScale()
+    print layer.isVisible()
+    mapcontext = gvsig.currentView().getMapContext()
+    print layer.isWithinScale(mapcontext.getScaleView())
+    
+    
